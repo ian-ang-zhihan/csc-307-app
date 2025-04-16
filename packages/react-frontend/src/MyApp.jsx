25d31c346e7587abe577eb8061c1f38fb809a567
Ian Ang <iannangg02@gmail.com>
2025-04-16 10:47:44 -0700

    IE3: Link FE to BE (3)

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -19,6 +19,18 @@ function MyApp() {
             })
     }, []);
 
+    function postUser(person) {
+        const promise = fetch("http://localhost:8000/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(person)
+        });
+
+        return promise;
+    }
+
     function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
             return i !== index;
@@ -27,7 +39,11 @@ function MyApp() {
     };
 
     function updateList(person) {
-        setCharacters([...characters, person]);
+        postUser(person)
+            .then(() => setCharacters([...characters, person]))
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
     return (
